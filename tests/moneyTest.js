8a66fcaa8d923d894eea059a592da90ec8fda582
GeovaniPassos <geovanipassos95@gmail.com>
2025-12-02 22:12:27 -0300

    Removido os testes padrões e implementado o moneyTest.js

--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -1,12 +1,25 @@
 import { formatCurrency } from '../scripts/utils/money.js'
 
+console.log('test suite: formatCurrency');
+
 if (formatCurrency(2095) === '20.95') {
     console.log('passed');
 } else {
     console.log('failed');
 }
-console.log(formatCurrency(Math.round(295)));
+
+console.log('Works with 0');
 
 if (formatCurrency(0) === '0.00') {
-    
+    console.log('passed');
+} else {
+    console.log('failed');
+}
+
+console.log('rounds up to the nearest cent');
+
+if (formatCurrency(2000.5) === '20.01') {
+    console.log('passed');
+} else {
+    console.log('failed');
 }
